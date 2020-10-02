557405ebfbfbf62691be4de72bac40ffa7dfabd6
jonathan.samara <jonathan.samara@econeteditora.com.br>
2020-10-02 13:12:20 -0300

    fazendo o welcome

---
+++ src/components/WelcomeStructure/WelcomeStructure.js
@@ -0,0 +1,10 @@
+import backFig from "./assets/backgroundimage.jpg"
+export default {
+    data() {
+        return {
+            generalState:"welcome",
+            backFig:backFig,
+            content:{}
+        } 
+    }
+}
--- src/components/WelcomeStructure/WelcomeStructure.vue
+++ src/components/WelcomeStructure/WelcomeStructure.vue
@@ -1,11 +1,17 @@
 <template>
     <div class="welcome">
-        <center>
-            <h1 class="specific-h1">Seja Bem Vindo</h1>
-        </center>
-    </div>
+        <v-parallax class="spefig" dark height="1000" :src="backFig" >
+            <div class="contofh1">
+                <center>
+                    <h1 class="specific-h1">Seja Bem Vindo</h1>
+                </center>
+            </div>
+        </v-parallax>
+    </div>        
 </template>
 
 <style lang="sass">
   @import "./WelcomeStructure.scss"
 </style>
+
+<script src="./WelcomeStructure.js"></script>
